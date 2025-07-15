2a1989695a382c64571b54304df6ea046c1fe749
Puja <pb2853313@gmail.com>
2025-07-16 00:55:04 +0530

    Save current changes before pull

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 
-def home_view(request):
-    return render(request, 'website/home.html')
+def index_view(request):
+    return render(request, 'website/index.html')
 
